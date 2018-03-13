4fae6103952da5797f30fa698d800ef49f867832
danielvfo <danielvfo@gmail.com>
2018-03-12 23:52:47 -0300

    Added BooksAPI and refactored some of the ListBooks.js code to use React Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI';
 import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
 import './App.css';
@@ -12,7 +12,14 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books});
+    });
   }
 
   setPage = (bool) => {
@@ -29,6 +36,7 @@ class BooksApp extends React.Component {
         ) : (
           <ListBooks
             onClickPlus={this.setPage}
+            books={this.state.books}
           />
         )}
       </div>
